6d42272e57fa1f5ae75729f45fa653368527f4af
lintu.gogi <lintu.joseph06@gmail.com>
2024-06-06 01:03:07 -0400

    New changes

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-let num;
+let num=350;
 let getBtn;
+let noOfGuess=0;
+let success=0;
 getBtn=document.getElementById("windowBtn");
-getBtn.addEventListener("click", getNumber());
+getBtn.addEventListener("click",()=> getNumber());
+
 function getNumber(){
-    num= window.alert();
-     console.log(num);
-    
+    num= window.prompt("Enter a number");
+    if(num==10){
+        window.alert("Right Number, Game Over");
+        getBtn.setAttribute('disabled','')
+        success=1;
     }
+    else
+    window.alert("Wrong number");
+     console.log(num);
+     if(noOfGuess<5 && success==0){
+        window.alert("You have more guesses, Click the button again to guess");
+        console.log("Total number of Guesses = "+noOfGuess);
+    }
+    if(noOfGuess==5){
+        getBtn.setAttribute('disabled','')
+        window.alert("No more guesses");
+    }
+    noOfGuess=noOfGuess+1;
+    }
+    
