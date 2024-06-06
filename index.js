84342fcdc701446372451f934ab25f20544cbb1e
lintu.gogi <lintu.joseph06@gmail.com>
2024-06-05 23:53:24 -0400

    First Commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let num;
+let getBtn;
+getBtn=document.getElementById("windowBtn");
+getBtn.addEventListener("click", getNumber());
+function getNumber(){
+    num= window.alert();
+     console.log(num);
+    
+    }
